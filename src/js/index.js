bb5cd2158be4375dcf7858000820a959c949515c
--replace-all <contactwillswebsites@gmail.com>
2022-08-08 12:37:29 -0600

    Developing standard template

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+// Header intersection observer - add class scrolled once scrolled
+
+
+
+// Fix header scrolling on anchor tag
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      let hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        1200,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
