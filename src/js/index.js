708748dafc88b970c72af08e7a436ca358aceee4
--replace-all <contactwillswebsites@gmail.com>
2022-08-09 20:44:08 -0600

    Kinda working, something to restore if needed

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,46 @@
-// Header intersection observer - add class scrolled once scrolled
-const header = document.querySelector('.header');
+let headerHeight;
+console.log(headerHeight, 'initial');
 
-const headerReveal = function (entry, headerObserver) {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.remove('hidden');
-    headerObserver.unobserve(entry.target);
-}
+const getHeaderHeight = function () {
+    headerHeight = document.querySelector("header").getBoundingClientRect().height;
+    console.log(headerHeight, 'run function');
+    return headerHeight
+};
 
-const headerObserver = new IntersectionObserver(headerReveal, {
-  root: null,
-  threshold: .2,
+$(document).ready(function() {
+  getHeaderHeight()
+  console.log('executed')
 });
+const init = function() {
+  $('<div id="headerView" aria-hidden="true"/>').prependTo("body");
+  getHeaderHeight();
+}
+init();
 
-header.classList.add('hidden');
-headerObserver.observe(header);
+//Observer options
+const resizeOptions = {
+  threshold: 1,
+};
 
-// const observer = new IntersectionObserver(
-//   ([entry]) => {
-//     if (entry.boundingClientRect.top < 0) {
-//       if (entry.isIntersecting) {
-//         // entered viewport at the top edge, hence scroll direction is up
-//       } else {
-//         // left viewport at the top edge, hence scroll direction is down
-//       }
-//     }
-//   },
-//   {
-//     root: rootElement,
-//   },
-// );
-
-
+//Activate observer for added item
+const resizeObserver = new IntersectionObserver((entries) => {
+  entries.forEach(function callbackFN(entry) {
+    //Once visible
+    if (entry.isIntersecting) {
+      document.getElementsByClassName("header")[0].classList.remove("scrolled"); //remove when item is visible
+    } else {
+      document.getElementsByClassName("header")[0].classList.add("scrolled"); //Add when item is not visible
+    }
+  });
+}, resizeOptions);
+resizeObserver.observe(document.querySelector("#headerView"));
 
 // Fix header scrolling on anchor tag
 $(document).ready(function () {
+  const headerHeight = getHeaderHeight();
+  console.log(headerHeight, 'main')
+
+  $('main').css('padding-top', headerHeight)
   // Add smooth scrolling to all links
   $("a").on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -47,14 +54,40 @@ $(document).ready(function () {
       // Using jQuery's animate() method to add smooth page scroll
       $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top,
+          scrollTop: $(hash).offset().top - headerHeight,
         },
-        1200,
+        800,
         function () {
           // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         }
       );
-    } // End if
+    }
   });
+});
+
+
+//On load of site going to the anchor tag
+$(window).on('pageshow',function(){ 
+  const hash = window.location.hash
+  
+  if (hash == '' || hash == '#' || hash == undefined) return false;
+    const target = $(hash);
+  
+    if (target.length) {
+    let headerHeight = getHeaderHeight();
+    $('html,body').stop().animate({
+        scrollTop: target.offset().top - headerHeight //offsets for fixed header
+        }, {
+        complete: function() { //ensures that it goes all the way to the anchor link
+            headerHeight = getHeaderHeight();
+            if(document.body.scrollTop != headerHeight || document.documentElement.scrollTop != headerHeight){
+                $('html,body').animate({
+                    scrollTop: target.offset().top - headerHeight //offsets for fixed header
+                }, 1);
+            }
+            $('html,body').stop(true, true);
+        }
+    });
+  }
 });
