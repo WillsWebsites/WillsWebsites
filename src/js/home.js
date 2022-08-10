6d69855d6fd7349ebc924fef068898eff6624676
--replace-all <contactwillswebsites@gmail.com>
2022-08-10 08:41:21 -0600

    First version of website starting template

--- src/js/home.js
+++ src/js/home.js
@@ -1,28 +1,28 @@
-// Services Observer
+// // Services Observer
 
-const services = document.querySelectorAll(".service");
+// const services = document.querySelectorAll(".service");
 
-const serviceReveal = function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) return;
+// const serviceReveal = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) return;
 
-    entry.target.style.opacity = 1;
-    entry.target.style.transform = "translateY(0)";
-    entry.target.style.boxShadow = "-1px 2px 0px rgba(238, 237, 237, .5)";
-    observer.unobserve(entry.target);
-  });
-};
+//     entry.target.style.opacity = 1;
+//     entry.target.style.transform = "translateY(0)";
+//     entry.target.style.boxShadow = "-1px 2px 0px rgba(238, 237, 237, .5)";
+//     observer.unobserve(entry.target);
+//   });
+// };
 
-const serviceObserver = new IntersectionObserver(serviceReveal, {
-  root: null,
-  threshold: 0.3,
-});
+// const serviceObserver = new IntersectionObserver(serviceReveal, {
+//   root: null,
+//   threshold: 0.3,
+// });
 
-services.forEach(function (service, i) {
-  serviceObserver.observe(service);
-  service.style.opacity = 0;
-  service.style.transform = "translateY(50px)";
-  service.style.transition = "all .5s ease-in";
-  service.style.transitionDelay = `.${i}s`;
-  service.style.boxShadow = "-1px 2px 0px rgba(238, 237, 237, 0)";
-});
+// services.forEach(function (service, i) {
+//   serviceObserver.observe(service);
+//   service.style.opacity = 0;
+//   service.style.transform = "translateY(50px)";
+//   service.style.transition = "all .5s ease-in";
+//   service.style.transitionDelay = `.${i}s`;
+//   service.style.boxShadow = "-1px 2px 0px rgba(238, 237, 237, 0)";
+// });
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,17 @@
 // Get header height for anchor support
+//prettier-ignore
 let headerHeight = document.querySelector('.header').getBoundingClientRect().height;
 
-const getHeaderHeight = function() {
+const getHeaderHeight = function () {
+  //prettier-ignore
   headerHeight = document.querySelector('.header').getBoundingClientRect().height;
   return headerHeight;
-}
-
-const clickHandler = function(target) {
+};
+const scrollHandler = function (target) {
   if (target.length) {
     headerHeight = getHeaderHeight();
+    //prettier-ignore
     $('html,body').stop().animate({
-      // offsets for fixed header
       scrollTop: target.offset().top - headerHeight
       }, {
         // ensures that it goes all the way to the anchor link
@@ -19,7 +20,6 @@ const clickHandler = function(target) {
           //prettier-ignore
           if(document.body.scrollTop !== headerHeight || document.documentElement.scrollTop !== headerHeight) {
             $('html,body').animate({
-              // offsets for fixed header
               scrollTop: target.offset().top - headerHeight
             }, 1);
           }
@@ -27,55 +27,55 @@ const clickHandler = function(target) {
         }
     });
   }
-}
+};
 
-document.querySelector('main').style.paddingTop = `${headerHeight}px`;
+document.querySelector("main").style.paddingTop = `${headerHeight}px`;
 
 //Anchor tag click implementation
 $('a[href*="#"]:not([href="#"])').click(function (e) {
-  const urlPath = this.href.substring(0, this.href.indexOf('#')); // url wthout hash
+  const urlPath = this.href.substring(0, this.href.indexOf("#")); // url wthout hash
   let locationPath = location.href;
 
-  if (locationPath.includes('#')) {
-    locationPath = location.href.substring(0, location.href.indexOf('#')); // url wthout hash
+  if (locationPath.includes("#")) {
+    locationPath = location.href.substring(0, location.href.indexOf("#")); // url wthout hash
   }
   // If the urls are the same then do the anchor animation
   if (urlPath == locationPath) {
     const target = $(this.href.substring(this.href.indexOf("#")));
-    clickHandler(target);
+    scrollHandler(target);
   }
   // Close mobile nav for on page anchors, if clicked in the navigation
-  if($(this).closest('ul').hasClass('menu-list')) {
+  if ($(this).closest("ul").hasClass("menu-list")) {
     $('input[type="checkbox"]').trigger("click");
   }
 });
 
 // Go to the anchor tag on site load
-$(window).on('pageshow',function() { 
+$(window).on("pageshow", function () {
   const hash = window.location.hash;
   //prettier-ignore
   if (hash == '' || hash == '#' || hash == undefined) return false;
 
   const target = $(hash);
-  clickHandler(target);
+  scrollHandler(target);
 });
 
 // Header observer
-$('<div id="headerView" aria-hidden="true"/>').prependTo('body');
-const headerView = document.getElementById('headerView');
+$('<div id="headerView" aria-hidden="true"/>').prependTo("body");
+const headerView = document.getElementById("headerView");
 
 const headerOptions = {
   threshold: 1,
-  rootMargin: '50px'
+  rootMargin: "50px",
 };
 
 const headerReveal = ([entry]) => {
-    if (entry.isIntersecting) {
-      $('.header').removeClass('scrolled');
-    } else {
-      $('.header').addClass('scrolled');
-    }
-  };
+  if (entry.isIntersecting) {
+    $(".header").removeClass("scrolled");
+  } else {
+    $(".header").addClass("scrolled");
+  }
+};
 
 const headerObserver = new IntersectionObserver(headerReveal, headerOptions);
 
