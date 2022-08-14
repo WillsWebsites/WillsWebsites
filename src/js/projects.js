4b17e28f622c5a3a75e3943c26637c9752447739
--replace-all <contactwillswebsites@gmail.com>
2022-08-14 11:34:45 -0600

    First part of projects piece complete minus touchups and effects

--- src/js/projects.js
+++ src/js/projects.js
@@ -38,7 +38,7 @@ const projectReveal = function(entries, observer) {
 
 const projectObserver = new IntersectionObserver(projectReveal, {
     root: null,
-    threshold: .35,
+    threshold: .25,
 });
 
 projects.forEach((project, i) => {
@@ -47,3 +47,54 @@ projects.forEach((project, i) => {
     project.style.transitionDelay = `${i % 2 === 0 ?  .2 + (i * .03) : .4 + (i * .02)}s`;
     projectObserver.observe(project);
 });
+
+$(document).ready(function() {
+    const containerHeight = document.querySelector('.project-intro .container').getBoundingClientRect().height;
+    document.querySelector('.project-intro .container .project-info-right').style.minHeight = `${containerHeight * .8}px`;
+
+
+    setTimeout(() => {
+        const websitesBar = document.getElementById('websites-bar');
+        const websiteCount = document.getElementById('website-count');
+        let height = 1;
+        let adjustHeight = setInterval(frame, .5);
+        function frame() {
+            if (height >= 100) {
+                    clearInterval(adjustHeight);
+            } else if (height >= 97) {
+                height += .02;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 90) {
+                height += .05;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 80) {
+                height += .1;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 70) {
+                height += .2;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 60) {
+                height += .3;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 50) {
+                height += .4;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else if (height >= 40) {
+                height += .5;
+                websitesBar.style.height = height + '%';
+                websiteCount.textContent = Math.floor(height);
+            } else {
+                height += .8; 
+                websitesBar.style.height = height + '%'; 
+                websiteCount.textContent = Math.floor(height);
+            }
+        }
+    }, 750);
+});
+
