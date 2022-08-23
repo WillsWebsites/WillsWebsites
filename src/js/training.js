5c1379a6756b6bb6cfedc576d4aac39b283f556e
--replace-all <contactwillswebsites@gmail.com>
2022-08-23 12:35:52 -0600

    Adding more to the training projects

--- src/js/training.js
+++ src/js/training.js
@@ -1,28 +1,61 @@
 // Training project Modal
 const modalHandler = function () {
-  if (window.matchMedia('(min-width: 992px)').matches) {
+  if (window.matchMedia("(min-width: 992px)").matches) {
     // Opening Modal and setting content
-    const modal = $('.training-modal');
-    const modalBg = $('.m-background');
-    const modalClose = $('.m-close');
-    let modalTitle = $('.m-title');
-    let modalDescription = $('.m-description');
+    const modal = $(".training-modal");
+    const modalBg = $(".m-background");
+    const modalClose = $(".m-close");
+    let modalTitle = $(".m-title");
+    let modalDescription = $(".m-description");
+    let modalProject = $(".m-project");
+    let modalRepo = $(".m-repo");
+    let modalJS = $(".m-js");
+    let modalReact = $(".m-react");
 
-    $('.training-item').click(function (e) {
-      $(modal).addClass('modal-opened');
+    const modalContentHandler = function (
+      title,
+      description,
+      project,
+      repo,
+      builtWith
+    ) {
+      $('.m-build i').addClass('m-hidden');
+      $(modalTitle).text(title);
+      $(modalDescription).text(description);
+      $(modalProject).attr('href', project);
+      $(modalRepo).attr('href', repo);
+      $(builtWith === 'JS' ? modalJS : modalReact).removeClass('m-hidden');
+    };
 
-      if ($(e.target).hasClass('guess-number')) {
-        $(modalTitle).text('Guess Number');
-        $(modalDescription).text('The primary use would be to...');
+    $(".training-item").click(function (e) {
+      $(modal).addClass("modal-opened");
+
+      switch (true) {
+        case $(e.target).closest(".training-item").hasClass("guess-number"):
+          modalContentHandler(
+            "Guess Number",
+            "The primary use would be to...",
+            "JS"
+          );
+          break;
+        case $(e.target).closest(".training-item").hasClass("pig-game"):
+          modalContentHandler(
+            "Pig Game",
+            "The primary use would be to...",
+            "./pig-game.html",
+            "https://github.com/WillsWebsites/wills-websites/tree/main/sample-projects/pig-game",
+            "JS"
+          );
+          break;
       }
     });
 
     // Closing Modal and clearing content
 
     const hideModal = function () {
-      $(modal).removeClass('modal-opened');
-      $(modalTitle).text('');
-      $(modalDescription).text('');
+      $(modal).removeClass("modal-opened");
+      $(modalTitle).text("");
+      $(modalDescription).text("");
     };
     $(modalClose).click(hideModal);
     $(modalBg).click(hideModal);
