77f4de15776e2b8244400cb460b727245719ef5f
--replace-all <contactwillswebsites@gmail.com>
2022-08-24 15:00:24 -0600

    Updating projects

--- src/js/training.js
+++ src/js/training.js
@@ -48,7 +48,7 @@ const modalHandler = function () {
         case $(e.target).closest('.training-item').hasClass('one-n-done'):
           modalContentHandler(
             `One-N'-Done`,
-            'The primary use would be to...',
+            [`A 2-player game to see who can get to 100 points first. But be careful, if you roll a 1 then any points you haven't held will be lost!`, `The goal is to be the first player to 100 points. You accumulate points by rolling the dice and then 'holding' those points. However if you roll a '1' then whatever points you haven't held will be lost and it will switch to the other players turn. Don't get too greedy!`, `How to manipulate things in the DOM based on random numbers. Tied in user input based on different portions of logic and provided feedback to the user.`],
             'one-n-done',
             'https://github.com/WillsWebsites/wills-websites/tree/main/sample-projects/one-n-done',
             'JS'
