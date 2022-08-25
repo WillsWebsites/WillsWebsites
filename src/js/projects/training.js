49c5b536ad7f3341eee7e83bc98c8c2c98042b2e
--replace-all <contactwillswebsites@gmail.com>
2022-08-24 18:53:23 -0600

    A few changes to README and file paths

--- src/js/projects/training.js
+++ src/js/projects/training.js
@@ -118,7 +118,7 @@ const modalHandler = function () {
           break;
         case $(e.target).closest('.training-item').hasClass('team-styler'):
           modalContentHandler(
-            'Team styler',
+            'Team Styler',
             [`A way to neatly display your team members and will eventually include various features to add more members and sections, and have a lot more adjustable settings.`, `As the project is under construction, for now you can click on the settings cog wheel on the upper right of any team section and then choose a team style to apply to the team members. You can also click on the add section button (+) to select a new section, although it will not add it to the section list for now. Many more features to come!`, `How to build a React application from scratch and structure/organize it in a convenient and reusable way. Also added a model to adjust settings and apply css based on user input. Implemented various hooks and modern React features.`],
             'team-styler',
             'https://github.com/WillsWebsites/wills-websites/tree/main/sample-projects/team-styler',
