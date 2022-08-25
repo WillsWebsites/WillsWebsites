1f540e8dff14c9270c8f7caae7607c924fdfd220
--replace-all <contactwillswebsites@gmail.com>
2022-08-24 23:02:01 -0600

    Adding more to projects

--- sample-projects/forkify/js/views/PageView.js
+++ sample-projects/forkify/js/views/PageView.js
@@ -55,12 +55,12 @@ class PageView extends View {
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-left"></use>
                 </svg>
-                <span>Page ${curPage - 1}</span>
+                <span>Page ${curPage ? curPage - 1 : ''}</span>
             </button>
             <button data-goto="${
               curPage + 1
             }" class="btn--inline pagination__btn--next">
-                <span>Page ${curPage + 1}</span>
+                <span>Page ${curPage ? curPage + 1 : ''}</span>
                 <svg class="search__icon">
                 <use href="${icons}#icon-arrow-right"></use>
                 </svg>
--- sample-projects/wills-meals/README.md
+++ sample-projects/wills-meals/README.md
@@ -1,6 +1,6 @@
 # Wills Meals
 ## A food ordering application that allows you to add items to your cart and adjust things in the cart.
-![Expense Tracker Preview](../../src/img/projects/previews/expense-tracker-preview.png)
+![Expense Tracker Preview](../../src/img/projects/previews/wills-meals-preview.png)
 ## Instructions
 ### Add items to your cart in whatever quanitites you'd like. Then you can access the cart and update any items if needed and all the totals will be calculated for you. Nothing happens when you checkout for now but more features to be added soon!
 ## Project Takeaways:
