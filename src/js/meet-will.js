25da56898adb624b5d80eb18e7c232dbb8a02108
--replace-all <contactwillswebsites@gmail.com>
2022-09-01 12:55:38 -0600

    Adjusting Resume

--- src/js/meet-will.js
+++ src/js/meet-will.js
@@ -4,121 +4,39 @@ const resumeItems = document.querySelectorAll(".resume-item");
 const categoryItems = document.querySelectorAll(".category-item");
 
 const resumeReveal = function (entries) {
-  entries.forEach((entry) => {
+  entries.forEach(entry => {
     // If entry isn't intersecting then don't account for it
     if (!entry.isIntersecting) return;
 
     let current;
 
+    // Remove start and active class to begin
     $(".category-item").removeClass("scroll--start");
     $(".category-item").removeClass("scroll--active");
 
-    entry.target.classList.add("scroll--active");
-
+    // Check for each item, add the active class to whichever is instersecting and set current variable to that item
     switch (true) {
       case entry.target.classList.contains("work"):
         $(".work").addClass("scroll--active");
-        current = 'work';
+        current = "work";
         break;
       case entry.target.classList.contains("education"):
         $(".education").addClass("scroll--active");
-        current = 'education'
+        current = "education";
         break;
       case entry.target.classList.contains("skills"):
         $(".skills").addClass("scroll--active");
-        current = 'skills'
+        current = "skills";
         break;
       case entry.target.classList.contains("personal"):
         $(".personal").addClass("scroll--active");
-        current = 'personal'
+        current = "personal";
         break;
     }
-    
-    $(`.category-item:not(.${current}), .resume-item:not(.${current})`).removeClass('scroll--active');
 
-    // $('.work').removeClass('scroll--start');
-
-    // window.onscroll = function(e) {
-
-    //   // If scrolling down
-    //   if (this.oldScroll < this.scrollY) {
-    //     switch(true) {
-    //       case entry.target.classList.contains('work'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.work').addClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('education'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.education').addClass('scroll--active');
-    //         $('.work').removeClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('skills'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.skills').addClass('scroll--active');
-    //         $('.work').removeClass('scroll--active');
-    //         $('.education').removeClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('personal'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.personal').addClass('scroll--active');
-    //         $('.work').removeClass('scroll--active');
-    //         $('.education').removeClass('scroll--active');
-    //         $('.skills').removeClass('scroll--active');
-    //         break;
-    //     }
-    //   } else {
-    //     switch(true) {
-    //       case entry.target.classList.contains('work'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.work').addClass('scroll--active');
-    //         $('.education').removeClass('scroll--active');
-    //         $('.skills').removeClass('scroll--active');
-    //         $('.personal').removeClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('education'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.education').addClass('scroll--active');
-    //         $('.skills').removeClass('scroll--active');
-    //         $('.personal').removeClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('skills'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.skills').addClass('scroll--active');
-    //         $('.personal').removeClass('scroll--active');
-    //         break;
-    //       case entry.target.classList.contains('personal'):
-    //         $('.category-item').removeClass('scroll--active');
-    //         $('.personal').addClass('scroll--active');
-    //         break;
-    //     }
-    //   }
-
-    //   this.oldScroll = this.scrollY;
-    // }
-
-    // if (entry.isIntersecting) {
-    //   entry.target.setAttribute("data-isin", "1");
-    // } else entry.target.removeAttribute("data-isin");
-
-    // if (!entry.isIntersecting) return;
-
-    // // $('.category-item').removeClass('scroll--active')
-    // for (i = 0; i < resumeItems.length; i++) {
-    //   console.log(resumeItems[i]);
-    //   if (resumeItems[i].getAttribute("data-isin")) {
-    //     if (
-    //       resumeItems[i - 1]?.hasAttribute("data-isin") ||
-    //       resumeItems[i + 1]?.hasAttribute("data-isin")
-    //     ) {
-    //       resumeItems[i - 1]?.removeAttribute("data-isin");
-    //       resumeItems[i + 1]?.removeAttribute("data-isin");
-    //     }
-    //     resumeItems[i].setAttribute("data-isin", "1");
-    //     break;
-    //   }
-    // }
-
-    // entry.target.classList.add("scroll--active");
+    // Remove the active class from each item that isn't the current one to make sure there is only one active at a time.
+    // prettier-ignore
+    $(`.category-item:not(.${current}), .resume-item:not(.${current})`).removeClass("scroll--active");
   });
 };
 
@@ -127,6 +45,6 @@ const projectObserver = new IntersectionObserver(resumeReveal, {
   threshold: 1,
 });
 
-resumeItems.forEach((item, i) => {
+resumeItems.forEach(item => {
   projectObserver.observe(item);
 });
