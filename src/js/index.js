bb5ee347676ed95ab464da6970b39e530ebf4f8b
--replace-all <contactwillswebsites@gmail.com>
2022-09-01 21:54:05 -0600

    Adding logo

--- src/js/index.js
+++ src/js/index.js
@@ -7,27 +7,27 @@ const getHeaderHeight = function () {
   headerHeight = document.querySelector('.header').getBoundingClientRect().height;
   return headerHeight;
 };
-// const scrollHandler = function (target) {
-//   if (target.length) {
-//     headerHeight = getHeaderHeight();
-//     //prettier-ignore
-//     $('html,body').stop().animate({
-//       scrollTop: target.offset().top - headerHeight
-//       }, {
-//         // ensures that it goes all the way to the anchor link
-//         complete: function() {
-//           headerHeight = getHeaderHeight();
-//           //prettier-ignore
-//           if(document.body.scrollTop !== headerHeight || document.documentElement.scrollTop !== headerHeight) {
-//             $('html,body').animate({
-//               scrollTop: target.offset().top - headerHeight
-//             }, 1);
-//           }
-//           $('html,body').stop(true, true);
-//         }
-//     });
-//   }
-// };
+const scrollHandler = function (target) {
+  if (target.length) {
+    headerHeight = getHeaderHeight();
+    //prettier-ignore
+    $('html,body').stop().animate({
+      scrollTop: target.offset().top - headerHeight
+      }, {
+        // ensures that it goes all the way to the anchor link
+        complete: function() {
+          headerHeight = getHeaderHeight();
+          //prettier-ignore
+          if(document.body.scrollTop !== headerHeight || document.documentElement.scrollTop !== headerHeight) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - headerHeight
+            }, 1);
+          }
+          $('html,body').stop(true, true);
+        }
+    });
+  }
+};
 
 $(".page-meet-will .digital-resume a").on("load scroll touchend click resize", function () {
   const locationPath = this.href;
@@ -44,14 +44,14 @@ $(".page-meet-will .digital-resume a").on("load scroll touchend click resize", f
 
       //prettier-ignore
       $('html,body').stop().animate({
-        scrollTop: target.offset().top - pageCenter 
+        scrollTop: target.offset().top - pageCenter - 20
         }, {
           // ensures that it goes all the way to the anchor link
           complete: function() {
             //prettier-ignore
             if(document.body.scrollTop !== headerHeight || document.documentElement.scrollTop !== headerHeight) {
               $('html,body').animate({
-                scrollTop: target.offset().top - pageCenter 
+                scrollTop: target.offset().top - pageCenter - 20
               }, 1);
             }
             $('html,body').stop(true, true);
@@ -85,34 +85,34 @@ $(".page-meet-will .digital-resume a").on("load scroll touchend click resize", f
   }
 });
 
-//Anchor tag click implementation
-// $('a[href*="#"]:not([href="#"])').click(function (e) {
-//   const urlPath = this.href.substring(0, this.href.indexOf("#")); // url wthout hash
-//   let locationPath = location.href;
+// Anchor tag click implementation
+$('body:not(.page-meet-will) a[href*="#"]:not([href="#"])').click(function (e) {
+  const urlPath = this.href.substring(0, this.href.indexOf("#")); // url wthout hash
+  let locationPath = location.href;
 
-//   if (locationPath.includes("#")) {
-//     locationPath = location.href.substring(0, location.href.indexOf("#")); // url wthout hash
-//   }
-//   // If the urls are the same then do the anchor animation
-//   if (urlPath == locationPath) {
-//     const target = $(this.href.substring(this.href.indexOf("#")));
-//     scrollHandler(target);
-//   }
-//   // Close mobile nav for on page anchors, if clicked in the navigation
-//   if ($(this).closest("ul").hasClass("menu-list")) {
-//     $('input[type="checkbox"]').trigger("click");
-//   }
-// });
+  if (locationPath.includes("#")) {
+    locationPath = location.href.substring(0, location.href.indexOf("#")); // url wthout hash
+  }
+  // If the urls are the same then do the anchor animation
+  if (urlPath == locationPath) {
+    const target = $(this.href.substring(this.href.indexOf("#")));
+    scrollHandler(target);
+  }
+  // Close mobile nav for on page anchors, if clicked in the navigation
+  if ($(this).closest("ul").hasClass("menu-list")) {
+    $('input[type="checkbox"]').trigger("click");
+  }
+});
 
 // Go to the anchor tag on site load
-// $(window).on("pageshow", function () {
-//   const hash = window.location.hash;
-//   //prettier-ignore
-//   if (hash == '' || hash == '#' || hash == undefined) return false;
+$(window).on("pageshow", function () {
+  const hash = window.location.hash;
+  //prettier-ignore
+  if (hash == '' || hash == '#' || hash == undefined) return false;
 
-//   const target = $(hash);
-//   scrollHandler(target);
-// });
+  const target = $(hash);
+  scrollHandler(target);
+});
 
 // Header observer
 $('<div id="headerView" aria-hidden="true"/>').prependTo("body");
