800c4fe5dcfc105bec5577f16a37b26cf7fbb0ae
--replace-all <contactwillswebsites@gmail.com>
2022-11-03 21:56:27 -0600

    Adjusting navigation for tablet

--- src/js/home.js
+++ src/js/home.js
@@ -1,8 +1,8 @@
 // Adding class to Home blobs on touchend to make sure background color is always filled in on touch
 
-if (window.matchMedia('(max-width: 991px)').matches) {
-    $('.hero-right .blob-btn').on('click touchstart', function(e) {
-        $('.blob-btn').removeClass('blob--active');
-        $(this).closest('.blob-btn').addClass('blob--active');
-    });
+if (window.matchMedia("(max-width: 991px)").matches) {
+  $(".hero-right .blob-btn").on("click touchstart", function (e) {
+    $(".blob-btn").removeClass("blob--active");
+    $(this).closest(".blob-btn").addClass("blob--active");
+  });
 }
